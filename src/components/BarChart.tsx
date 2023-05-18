d33d81755121b8bce4d8619b205dac10de07cd04
Ikram Maulana <ikram075@ummi.ac.id>
2023-05-18 09:57:36 +0700

    Feat: Rebuild Project
    Update:
    Rebuild project using nextjs 13.2 and new app router

--- src/components/BarChart.tsx
+++
@@ -1,7 +0,0 @@
-import { ArcElement, Chart } from "chart.js/auto";
-import { Bar } from "react-chartjs-2";
-Chart.register(ArcElement);
-
-export default function BarChart({ chartData }: any) {
-  return <Bar data={chartData} />;
-}
--- src/components/PieChart.tsx
+++
@@ -1,7 +0,0 @@
-import { ArcElement, Chart } from "chart.js/auto";
-import { Pie } from "react-chartjs-2";
-Chart.register(ArcElement);
-
-export default function PieChart({ chartData }: any) {
-  return <Pie data={chartData} />;
-}
